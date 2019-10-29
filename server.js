b96037d0501f6a679d23c89e6a9067e76d745742
Tom Cox <thomasxcox731@gmail.com>
2019-10-29 00:03:59 -0500

    Successfully connected mlab to robo3t. Will soon see if we can get successfully deployed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 // mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://thomasxaviercox731:zbikowski731@ds241308.mlab.com:41308/heroku_2z6ltfzt";
 
 mongoose.connect(MONGODB_URI);
 
