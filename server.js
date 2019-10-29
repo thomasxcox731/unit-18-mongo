24635cfc9c7ac71269de2a70061fff9c342838c7
Tom Cox <thomasxcox731@gmail.com>
2019-10-28 23:48:57 -0500

    mlab info added. will redeploy to see if this makes it work

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 //ROUTES
 
